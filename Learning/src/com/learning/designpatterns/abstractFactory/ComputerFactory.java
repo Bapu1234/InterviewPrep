36da981857e9a2eb8c8c03dfdf6bb6224e534931
Bapu Patil <bpsuryakanthpatil25@massmutual.com>
2020-09-17 17:07:28 -0400

    restructuring

--- Learning/src/com/learning/designpatterns/abstractFactory/ComputerFactory.java
+++
@@ -1,9 +0,0 @@
-package com.learning.designpatterns.abstractFactory;
-
-
-public class ComputerFactory {
-
-    public static Computer getComputer(AbstractFactory fact){
-        return fact.createComputer();
-    }
-}
